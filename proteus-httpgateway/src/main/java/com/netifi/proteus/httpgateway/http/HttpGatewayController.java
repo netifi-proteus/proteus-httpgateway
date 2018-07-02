e5f114d3a5208e58c51d17f384ac354f78c3d0d3
gregwhitaker <gwhitake@gmail.com>
2018-07-02 13:03:44 -0700

    Doing work

--- proteus-httpgateway/src/main/java/com/netifi/proteus/httpgateway/http/HttpGatewayController.java
+++ proteus-httpgateway/src/main/java/com/netifi/proteus/httpgateway/http/HttpGatewayController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
