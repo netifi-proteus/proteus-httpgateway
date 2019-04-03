c8be3b659117c65a5ec9efe0702100a1d44e9777
Oleh Dokuka <shadowgun@i.ua>
2019-04-04 00:45:42 +0300

    fixes scripts

--- buildtravis.sh
+++ buildtravis.sh
@@ -13,12 +13,12 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ] 
     echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     export ORG_GRADLE_PROJECT_commitHash=${TRAVIS_COMMIT::7}
-    ./gradlew -PversionSuffix=".BUILD-SNAPSHOT" clean build artifactoryPublish dockerPush --stacktrace
+    ./gradlew -PversionSuffix=".BUILD-SNAPSHOT" clean build artifactoryPublish pushImage --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == release/* ]] && [ "$TRAVIS_TAG" == "" ]; then
     # Release Branch
     echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']'
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    ./gradlew -PversionSuffix="-RC" clean build dockerPush --stacktrace
+    ./gradlew -PversionSuffix="-RC" clean build pushImage --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
     # Master Branch
     echo -e 'Build Master for Release => Branch ['$TRAVIS_BRANCH']'
@@ -30,9 +30,9 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     echo -e 'Build Tag for Release => Tag ['$TRAVIS_TAG']'
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     export ORG_GRADLE_PROJECT_isRelease=true
-    ./gradlew clean build bintrayUpload dockerPush --stacktrace --refresh-dependencies
+    ./gradlew clean build bintrayUpload pushImage --stacktrace --refresh-dependencies
 else
     # Feature Branch
     echo -e 'Build Branch => Branch ['$TRAVIS_BRANCH']'
-    ./gradlew -PversionSuffix=".BUILD-SNAPSHOT" clean build artifactoryPublish --stacktrace --refresh-dependencies
+    ./gradlew -PversionSuffix=".BUILD-SNAPSHOT" clean build artifactoryPublish pushImage --stacktrace --refresh-dependencies
 fi
